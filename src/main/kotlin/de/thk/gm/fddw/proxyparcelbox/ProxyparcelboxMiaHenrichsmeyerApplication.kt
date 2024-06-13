8279514fae82e3efc726ea5e592b47a5d79136bc
mia <mia_charlotte.henrichsmeyer@smail.th-koeln.de>
2024-06-13 11:49:03 +0200

    init

---
+++ src/main/kotlin/de/thk/gm/fddw/proxyparcelbox/ProxyparcelboxMiaHenrichsmeyerApplication.kt
@@ -0,0 +1,11 @@
+package de.thk.gm.fddw.proxyparcelbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProxyparcelboxMiaHenrichsmeyerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProxyparcelboxMiaHenrichsmeyerApplication>(*args)
+}
---
+++ src/test/kotlin/de/thk/gm/fddw/proxyparcelbox/ProxyparcelboxMiaHenrichsmeyerApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.thk.gm.fddw.proxyparcelbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProxyparcelboxMiaHenrichsmeyerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
