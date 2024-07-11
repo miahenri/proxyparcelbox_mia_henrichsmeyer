5c530bf9e00797e73f842fee7169a2d2c28dd29c
mia <mia_charlotte.henrichsmeyer@smail.th-koeln.de>
2024-07-11 11:30:34 +0200

    css erweitert

--- src/main/resources/static/scripts/chatroomScript.js
+++ src/main/resources/static/scripts/chatroomScript.js
@@ -1,69 +1,73 @@
-window.onload = function() {
-    const ws = new WebSocket("/chats");
-    const echo = document.querySelector('.messages');
+const ws = new WebSocket("/chats");
+const echo = document.querySelector('.messages');
+const trackingNumber = document.getElementById('trackingNumber').textContent.split(': ')[1];
+
+ws.onmessage = function (message) {
+    console.log("Nachricht angekommen: " + message.data);
+    let receivedDiv = document.createElement('div');
+    receivedDiv.classList.add('received');
+
+    let messageText = document.createElement('p');
+    messageText.textContent = message.data;
+    receivedDiv.appendChild(messageText);
+
+    let timestampSpan = document.createElement('span');
+    timestampSpan.classList.add('timestamp');
+    timestampSpan.textContent = getTimestamp();
+    receivedDiv.appendChild(timestampSpan);
+
+    echo.appendChild(receivedDiv);
+};
+
+function sendMessage() {
+    const input = document.querySelector('#messageText');
+    const messageText = input.value;
+
+    // let date = new Date();
+    let timestamp = new Date().toLocaleTimeString('de-DE', {hour: '2-digit', minute: '2-digit'});
+
+    const messageHtml = '<div class="sent">' +
+        '<p>' + messageText + '</p>' +
+        '<span class="timestamp">' + timestamp + '</span>' +
+        '</div>';
+
+    echo.innerHTML += messageHtml;
+
     const trackingNumber = document.getElementById('trackingNumber').textContent.split(': ')[1];
 
-    ws.onmessage = function (message) {
-        console.log("WebSocket message received: " + message.data);
-        let receivedDiv = document.createElement('div');
-        receivedDiv.classList.add('received');
-
-        let messageText = document.createElement('p');
-        messageText.textContent = message.data;
-        receivedDiv.appendChild(messageText);
-
-        let timestampSpan = document.createElement('span');
-        timestampSpan.classList.add('timestamp');
-        timestampSpan.textContent = getTimestamp();
-        receivedDiv.appendChild(timestampSpan);
-
-        echo.appendChild(receivedDiv);
+    const message = {
+        sender: 'User',
+        text: messageText
     };
 
-    function sendMessage() {
-        const input = document.querySelector('#messageText');
-        const messageText = input.value;
+    fetch('/chats/' + trackingNumber, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message)
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+    }).catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
 
-        let date = new Date();
-        let timestamp =  date.toLocaleTimeString('de-DE', {hour: '2-digit', minute: '2-digit'});
+    ws.send(messageText);
 
-        const messageHtml = '<div class="sent">' +
-            '<p>' + messageText + '</p>' +
-            '<span class="timestamp">' + timestamp + '</span>' +
-            '</div>';
+    input.value = '';
+}
 
-        echo.innerHTML += messageHtml;
+function getTimestamp() {
+    const currentTime = new Date();
+    return currentTime.toLocaleTimeString('de-DE', {hour: '2-digit', minute: '2-digit'});
+}
 
-        const trackingNumber = document.getElementById('trackingNumber').textContent.split(': ')[1];
-
-        const message = {
-            sender: 'User',
-            text: messageText
-        };
-
-        fetch('/chats/' + trackingNumber, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(message)
-        }).then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-        }).catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
-
-        ws.send(messageText);
-
-        input.value = '';
+document.getElementById('sending').addEventListener('click', sendMessage)
+document.querySelector('#messageText').addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        sendMessage();
+        event.preventDefault(); // Verhindert das Standardverhalten der Entertaste (z.B. neues Zeile in einem Textfeld)
     }
-
-    function getTimestamp() {
-        const currentTime = new Date();
-        return currentTime.toLocaleTimeString('de-DE', {hour: '2-digit', minute: '2-digit'});
-    }
-
-    document.getElementById('sending').addEventListener('click', sendMessage)
-};
+});
